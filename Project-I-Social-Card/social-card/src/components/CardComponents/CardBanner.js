df622773cda262a371cd680991ae1ca2d4061fef
azinoVo <azinovo22@gmail.com>
2019-04-23 17:18:58 -0500

    fixed text-decoration

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -4,7 +4,7 @@ import './Card.css';
 const CardBanner = function(){
     return (
         <div class='card-img'>
-            <img src='https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png' alt='React Logo' ></img>  
+            <img id='react-img' src='https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png' alt='React Logo' ></img>  
         </div>
     );
 };
