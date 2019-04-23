691141301524b4cd752b4b9911b9c05d85bba2dd
azinoVo <azinovo22@gmail.com>
2019-04-23 15:06:00 -0500

    setup HeaderContainer and components for HeaderContainer

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,44 @@
+//Example Header
+// import React from 'react';
+// import WeatherIcon from './WeatherIcon';
+// import Location from './Location';
+// import Temperature from './Temperature';
+// import Date from './Date';
+// import './card.scss';
+
+// const CardContainer = () => {
+//   return (
+//     <div className="card-container">
+//       <WeatherIcon />
+//       <Location />
+//       <Temperature />
+//       <Date />
+//     </div>
+//   );
+// };
+
+// export default CardContainer;
+
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => {
+    return (
+        <div className='header-container'>
+            <HeaderContent />
+            <HeaderTitle />
+            <ImageThumbnail />
+        </div>
+
+    );
+};
+
+
+
+
+
+
+
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+
+//Example
+// import React from 'react';
+
+// const Temperature = () => {
+//     return (
+//       <div className="temperature">
+//         <p>76°F</p>
+//       </div>
+//     );
+//   };
+  
+//   export default Temperature;
+
+export default HeaderContent;
